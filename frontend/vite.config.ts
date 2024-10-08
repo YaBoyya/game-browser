f5868167f6ceca4135e0963a754569583b49cd67
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-10-08 15:42:28 +0200

    Add frontend to docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()]
