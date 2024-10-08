f4f3a45758f1169e139483c90543f7627f7b5807
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-10-08 15:12:45 +0200

    Setup backend and database in docker compose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.BACKEND_PORT || 3000;
+const port = parseInt(process.env.BACKEND_DOCKER_PORT || "3000", 10);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Game Browser");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`[server]: Docker server is running at port: ${port}`);
+  console.log(`[server]: Locally server should be available at http://localhost:${process.env.BACKEND_LOCAL_PORT}`);
 });
