4f2617654f9a3e3c12b4b07c5f4ee6780ce09f25
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-11-17 19:26:58 +0100

    Add constants file and initial Register fetch

---
+++ frontend/src/constants.tsx
@@ -0,0 +1,7 @@
+// cookies
+export const COOKIE_TOKEN_NAME = "AUTH_TOKEN";
+
+// endpoints
+const APIURL = "http://localhost:8080"
+export const LOGINURL = `${APIURL}/login`;
+export const REGISTERURL = `${APIURL}/register`;
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import FormWrapper from "../components/wrappers/FormWrapper";
 import RenderForm from "../components/RenderForm";
+import { REGISTERURL } from "../constants";
 
 function Register() {
   const [credentials, setCredentials] = useState({
@@ -18,9 +19,47 @@ function Register() {
   };
 
   // TODO type it
-  const onSubmit = (event: any) => {
+  const onSubmit = async (event: any) => {
+    console.log("HI");
     event.preventDefault();
-    console.log("submit");
+    if(!Object.values(credentials).every((val: string) => val.trim() ? true : false)) {
+      // TODO error when data is empty
+      console.log(credentials)
+      return;
+    }
+
+    try {
+      // TODO clear errors
+      console.log("HI");
+      const controller = new AbortController();
+      const timeout = setTimeout(() => controller.abort(), 5000);
+
+      const response = await fetch(REGISTERURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        signal: controller.signal
+      });
+
+      clearTimeout(timeout);
+
+      if (!response.ok) {
+        throw new Error("Invalid credentials");
+      }
+      const data = await response.json();
+      console.log(JSON.stringify(data));
+    } catch (error: any) {
+      if (error.name === "AbortError") {
+        console.error("Connection timed out");
+      } else if (error.name === "TypeError") {
+        console.error(error.message);
+      } else {
+        // perhaps error to input error messages
+        console.error(error.message);
+      }
+      // TODO add method to clear object data
+    }
   }
 
   return(
