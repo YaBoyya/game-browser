0791197e4b3d7feac55db6ac2ecbd2ca79f35a11
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-11-17 19:27:30 +0100

    Fix and add input types to RenderForm

--- frontend/src/components/RenderForm.tsx
+++ frontend/src/components/RenderForm.tsx
@@ -3,13 +3,13 @@ function RenderForm({data, handleInput}: any) {
   return (
     <>
       {Object.keys(data).map((key, index) => (
-        <div className="flex flex-col my-3">
+        <div key={index} className="flex flex-col my-3">
           <label htmlFor={key}>{key[0].toUpperCase() + key.slice(1)}</label>
           <input
-            key={index}
             id={key}
             className="px-3 py-2"
-            placeholder={`Your ${key[0].toUpperCase() + key.slice(1)}`}
+            placeholder={`Your ${key}`}
+            type={["email", "password"].includes(key) ? key : "text"}
             onChange={(event) => handleInput(event.target.value, key)}
           />
         </div>
